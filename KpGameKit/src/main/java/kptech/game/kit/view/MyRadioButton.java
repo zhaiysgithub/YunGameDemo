607e394470b32d1bcfeff5b106fd5846296bf7f2
liang <18363838097@163.com>
2020-07-10 13:49:37 +0800

    增加支付宝支付 修改弹窗布局

---
+++ KpGameKit/src/main/java/kptech/game/kit/view/MyRadioButton.java
@@ -0,0 +1,76 @@
+package kptech.game.kit.view;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.drawable.Drawable;
+import android.util.AttributeSet;
+import android.widget.RadioButton;
+
+import kptech.game.kit.R;
+
+/**
+ * 可以设置图片大小的RadioButton
+ */
+
+
+public class MyRadioButton extends RadioButton {
+
+    private int mDrawableHeight;//xml文件中设置的高度
+    private int mDrawableWidth;//xml文件中设置的宽度
+
+    public MyRadioButton(Context context) {
+        this(context, null, 0);
+    }
+
+    public MyRadioButton(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public MyRadioButton(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        // TODO Auto-generated constructor stub
+        Drawable drawableLeft = null, drawableTop = null, drawableRight = null, drawableBottom = null;
+        TypedArray a = context.obtainStyledAttributes(attrs,
+                R.styleable.MyRadioButton);
+        setClickable(true);
+        int n = a.getIndexCount();
+        for (int i = 0; i < n; i++) {
+            int attr = a.getIndex(i);
+            if (attr == R.styleable.MyRadioButton_drawableHeight) {
+                mDrawableHeight = a.getDimensionPixelSize(R.styleable.MyRadioButton_drawableHeight, 50);
+            } else if (attr == R.styleable.MyRadioButton_drawableWidth) {
+                mDrawableWidth = a.getDimensionPixelSize(R.styleable.MyRadioButton_drawableWidth, 50);
+            } else if (attr == R.styleable.MyRadioButton_drawableTop) {
+                drawableTop = a.getDrawable(attr);
+            } else if (attr == R.styleable.MyRadioButton_drawableBottom) {
+                drawableRight = a.getDrawable(attr);
+            } else if (attr == R.styleable.MyRadioButton_drawableRight) {
+                drawableBottom = a.getDrawable(attr);
+            } else if (attr == R.styleable.MyRadioButton_drawableLeft) {
+                drawableLeft = a.getDrawable(attr);
+            }
+        }
+        a.recycle();
+
+        setCompoundDrawablesWithIntrinsicBounds(drawableLeft, drawableTop, drawableRight, drawableBottom);
+
+    }
+
+    public void setCompoundDrawablesWithIntrinsicBounds(Drawable left,
+                                                        Drawable top, Drawable right, Drawable bottom) {
+
+        if (left != null) {
+            left.setBounds(0, 0, mDrawableWidth, mDrawableHeight);
+        }
+        if (right != null) {
+            right.setBounds(0, 0, mDrawableWidth, mDrawableHeight);
+        }
+        if (top != null) {
+            top.setBounds(0, 0, mDrawableWidth, mDrawableHeight);
+        }
+        if (bottom != null) {
+            bottom.setBounds(0, 0, mDrawableWidth, mDrawableHeight);
+        }
+        setCompoundDrawables(left, top, right, bottom);
+    }
+}
