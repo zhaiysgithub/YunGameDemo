a943444856ac1ac9f4f93baac8a1887bda6bb21c
翟永帅 <zhaiyongshuai@kuaipantech.com>
2021-05-10 11:05:55 +0800

    添加 uninqueId 认证

--- app/src/main/java/com/yd/yunapp/gamebox/MainActivity.java
+++ app/src/main/java/com/yd/yunapp/gamebox/MainActivity.java
@@ -132,7 +132,7 @@ public class MainActivity extends AppCompatActivity {
             game.showAd = GameInfo.GAME_AD_SHOW_AUTO;
         }
 
-        boolean shouldLoginAuth = UserCertificationManager.getInstance().shouldLoginAuth(getApplication(), game.pkgName);
+        boolean shouldLoginAuth = UserCertificationManager.getInstance().shouldLoginAuthByPhone(getApplication(), "15711485499",game.pkgName);
         if (shouldLoginAuth){
             UserCertificationDialog mCertificationDialog = new UserCertificationDialog(MainActivity.this);
             mCertificationDialog.setOnCallback(new UserCertificationDialog.OnUserCerificationCallbck() {
